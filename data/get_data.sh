c1b0897719b95401e339840f765dabdbb09ad075
Patrick <patrick.bryant@live.com>
2020-11-19 10:40:12 +0100

    parsing data

---
+++ data/get_data.sh
@@ -0,0 +1,3 @@
+#Get the Oxford data
+wget https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv
+#Get the google mobility data
---
+++ data/preprocess.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import argparse
+import sys
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+import pdb
+#Arguments for argparse module:
+parser = argparse.ArgumentParser(description = '''Preprocess data for training.''')
+
+parser.add_argument('--oxford_file', nargs=1, type= str,
+                  default=sys.stdin, help = 'Path to oxford data file.')
+
+ser.add_argument('--outdir', nargs=1, type= str,
+                  default=sys.stdin, help = 'Path to output directory. Include /in end')
+
+
+
+#####MAIN#####
+args = parser.parse_args()
+oxford_data = pd.read_csv(args.oxford_file[0],
+                 parse_dates=['Date'],
+                 encoding="ISO-8859-1",
+                 dtype={"RegionName": str,
+                        "RegionCode": str},
+                 error_bad_lines=False)
+outdir = args.outdir[0]
+
+pdb.set_trace()
