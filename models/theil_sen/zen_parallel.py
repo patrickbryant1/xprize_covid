ad57804095f85b99cdba7e8a88abb90b533826ce
Patrick <patrick.bryant@live.com>
2020-11-27 09:47:05 +0100

    setting up parallel run

--- models/theil_sen/zen_parallel.py
+++ models/theil_sen/zen_parallel.py
@@ -197,6 +197,7 @@ adjusted_data = pd.read_csv(args.adjusted_data[0],
                         "Region_index":int},
                  error_bad_lines=False)
 adjusted_data = adjusted_data.fillna(0)
+days_ahead = args.days_ahead[0]
 outdir = args.outdir[0]
 
 #Get features
@@ -204,33 +205,12 @@ X_train,y_train,X_test,y_test,populations,regions  = get_features(adjusted_data,
 
 
 #Fit models
-try:
-    preds = np.load(outdir+'preds.npy',allow_pickle=True)
-except:
-    preds = []
-    coefficents = []
-    intercepts = []
-    breakdown_points = []
+pred,coefs,intercept,breakdown = fit_model(X_train,y_train[:,days_ahead-1],X_test)
+#Save
+np.save(outdir+'preds.npy', pred)
+np.save(outdir+'coefficients.npy',coefs)
+np.save(outdir+'intercept.npy',intercept)
+np.save(outdir+'breakdown_point.npy',breakdown)
+print(day,'error', np.round(np.average(np.absolute(pred-y_test[:,days_ahead-1]))))
 
-    for day in range(y_train.shape[1]):
-        pred,coefs,intercept,breakdown = fit_model(X_train,y_train[:,day],X_test)
-         #Save
-        preds.append(pred)
-        coefficents.append(coefs)
-        intercepts.append(intercept)
-        breakdown_points.append(breakdown)
-        print(day,'error', np.round(np.average(np.absolute(np.array(preds[-1])-y_test[:,day]))))
-    #Save
-    preds = np.array(preds)
-    np.save(outdir+'preds.npy', preds)
-    coefficients = np.array(coefficients)
-    np.save(outdir+'coefficients.npy',coefficients)
-    intercepts = np.array(intercepts)
-    np.save(outdir+'intercepts.npy',intercepts)
-    breakdown_points = np.array(breakdown_points)
-    np.save(outdir+'breakdown_points.npy',breakdown_points)
-    print('Done')
-
-#Evaluate fit
-evaluate(preds,y_test,outdir,regions,populations)
-pdb.set_trace()
+print('Done')
