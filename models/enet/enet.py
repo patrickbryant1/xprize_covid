43addbca41291fee964677cd30b3284ea60abc56
Patrick <patrick.bryant@live.com>
2020-11-30 15:03:22 +0100

    looking at results

--- models/enet/enet.py
+++ models/enet/enet.py
@@ -166,7 +166,7 @@ def split_for_training(sel):
 
     return np.array(X_train), np.array(y_train),np.array(X_test), np.array(y_test), np.array(populations), np.array(regions)
 
-def evaluate(preds,y_test,outdir,regions,populations):
+def evaluate(preds,coefs,intercepts,y_test,outdir,regions,populations):
     '''Evaluate the model
     '''
 
@@ -188,17 +188,17 @@ def evaluate(preds,y_test,outdir,regions,populations):
         total_regional_2week_mae.append(np.average(np.absolute(preds[:,ri][:14]-y_test[ri,:][:14])))
         region_corr = pearsonr(preds[:,ri],y_test[ri,:])[0]
         all_regional_corr.append(region_corr)
-        fig, ax = plt.subplots(figsize=(6/2.54, 4/2.54))
-        plt.plot(range(1,22),preds[:,ri],label='pred',color='grey')
-        plt.plot(range(1,22),y_test[ri,:],label='true',color='g')
-        plt.title(regions[ri]+'\nPopulation:'+str(np.round(populations[ri]/1000000,1))+' millions\nCumulative error:'+str(np.round(region_error))+' PCC:'+str(np.round(region_corr,2)))
-        plt.xlabel('Day')
-        plt.ylabel('Cases')
-        ax.spines['top'].set_visible(False)
-        ax.spines['right'].set_visible(False)
-        fig.tight_layout()
-        plt.savefig(outdir+'regions/'+regions[ri]+'.png',format='png')
-        plt.close()
+        # fig, ax = plt.subplots(figsize=(6/2.54, 4/2.54))
+        # plt.plot(range(1,22),preds[:,ri],label='pred',color='grey')
+        # plt.plot(range(1,22),y_test[ri,:],label='true',color='g')
+        # plt.title(regions[ri]+'\nPopulation:'+str(np.round(populations[ri]/1000000,1))+' millions\nCumulative error:'+str(np.round(region_error))+' PCC:'+str(np.round(region_corr,2)))
+        # plt.xlabel('Day')
+        # plt.ylabel('Cases')
+        # ax.spines['top'].set_visible(False)
+        # ax.spines['right'].set_visible(False)
+        # fig.tight_layout()
+        # plt.savefig(outdir+'regions/'+regions[ri]+'.png',format='png')
+        # plt.close()
         results_file.write(regions[ri]+': '+str(region_corr)+'\n')
 
     #Convert to arrays
@@ -218,6 +218,27 @@ def evaluate(preds,y_test,outdir,regions,populations):
     all_regional_corr[np.isnan(all_regional_corr)]=0
     results_file.write('Average correlation: '+str(np.average(all_regional_corr)))
     results_file.close()
+
+    #Look at coefs
+    #The first are repeats 21 times, then single_features follow: [country_index,region_index,death_to_case_scale,case_death_delay,gross_net_income,population_density,population]
+    #--> get the last features, then divide into 21 portions
+    single_feature_names=['country_index','region_index','death_to_case_scale','case_death_delay','gross_net_income','population_density','Change in last 21 days','pdi', 'idv', 'mas', 'uai', 'ltowvs', 'ivr','PC1','PC2','population']
+    #days pred,days behind - this goes from -21 to 1,features
+    repeat_feature_names = ['C1_School closing', 'C2_Workplace closing', 'C3_Cancel public events', 'C4_Restrictions on gatherings', 'C5_Close public transport', 'C6_Stay at home requirements',
+    'C7_Restrictions on internal movement', 'C8_International travel controls', 'H1_Public information campaigns', 'H2_Testing policy', 'H3_Contact tracing', 'H6_Facial Coverings',
+    'rescaled_cases', 'cumulative_rescaled_cases', 'monthly_temperature', 'retail_and_recreation', 'grocery_and_pharmacy', 'parks','transit_stations', 'workplaces', 'residential']
+    all_feature_names = single_feature_names+repeat_feature_names*21
+    for i in range(coefs.shape[0]):
+        fig,ax=plt.subplots(figsize=(18,6))
+        plt.bar(range(coefs.shape[1]),coefs[i,:],)
+        #for j in range(coefs.shape[1]):
+        #    plt.text(j,coefs[i,j],all_feature_names[j],fontsize=12)
+
+        plt.title('Day '+str(i+1))
+        plt.tight_layout()
+        plt.savefig(outdir+'coefs_'+str(i+1)+'.png',format='png',dpi=300)
+        plt.close()
+
 
 
 def fit_model(X_train,y_train,X_test):
@@ -279,4 +300,4 @@ except:
 
 pdb.set_trace()
 #Evaluate fit
-evaluate(preds,y_test,outdir,regions,populations)
+evaluate(preds,coefficients,intercepts,y_test,outdir,regions,populations)
