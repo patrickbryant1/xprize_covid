4ad0722aeee198ab3c2140ca252674109c01ae28
Patrick <patrick.bryant@live.com>
2020-12-01 12:52:46 +0100

    setting up dense net

---
+++ models/dense/dense_net.py
@@ -0,0 +1,336 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import argparse
+import sys
+import os
+import numpy as np
+import random
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import KFold
+
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow.keras.backend as K
+import tensorflow.keras.layers as L
+import tensorflow.keras.models as M
+
+from scipy.stats import pearsonr
+
+import pdb
+#Arguments for argparse module:
+parser = argparse.ArgumentParser(description = '''A dense regression model.''')
+
+parser.add_argument('--adjusted_data', nargs=1, type= str,
+                  default=sys.stdin, help = 'Path to processed data file.')
+parser.add_argument('--days_ahead', nargs=1, type= int,
+                  default=sys.stdin, help = 'Number of days ahead to fit')
+parser.add_argument('--start_date', nargs=1, type= str,
+                  default=sys.stdin, help = 'Date to start from.')
+parser.add_argument('--train_days', nargs=1, type= int,
+                  default=sys.stdin, help = 'Days to include in fitting.')
+parser.add_argument('--outdir', nargs=1, type= str,
+                  default=sys.stdin, help = 'Path to output directory. Include /in end')
+
+#######FUNCTIONS#######
+def seed_everything(seed=2020):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+
+def get_features(adjusted_data):
+    '''Get the selected features
+    '''
+
+    selected_features = ['C1_School closing',
+                        'C2_Workplace closing',
+                        'C3_Cancel public events',
+                        'C4_Restrictions on gatherings',
+                        'C5_Close public transport',
+                        'C6_Stay at home requirements',
+                        'C7_Restrictions on internal movement',
+                        'C8_International travel controls',
+                        'H1_Public information campaigns',
+                        'H2_Testing policy',
+                        'H3_Contact tracing',
+                        'H6_Facial Coverings', #These first 12 are the ones the prescriptor will assign
+                        'Country_index',
+                        'Region_index',
+                        'CountryName',
+                        'RegionName',
+                        'rescaled_cases',
+                        'cumulative_rescaled_cases',
+                        'death_to_case_scale',
+                        'case_death_delay',
+                        'gross_net_income',
+                        'population_density',
+                        'monthly_temperature',
+                        'retail_and_recreation',
+                        'grocery_and_pharmacy',
+                        'parks',
+                        'transit_stations',
+                        'workplaces',
+                        'residential',
+                        'pdi', 'idv', 'mas', 'uai', 'ltowvs', 'ivr',
+                        'population']
+
+    sel = adjusted_data[selected_features]
+
+    return sel
+
+def split_for_training(sel):
+    '''Split the data for training and testing
+    '''
+    X_train = [] #Inputs
+    y_train = [] #Targets
+    X_test = [] #Inputs
+    y_test = [] #Targets
+    countries = sel['Country_index'].unique()
+    populations = []
+    regions = []
+    for ci in countries:
+        country_data = sel[sel['Country_index']==ci]
+        #Check regions
+        country_regions = country_data['Region_index'].unique()
+        for ri in country_regions:
+            country_region_data = country_data[country_data['Region_index']==ri]
+            #Select data 14 days before 0 cases
+            try:
+                si = max(0,country_region_data[country_region_data['cumulative_rescaled_cases']>0].index[0]-14)
+                country_region_data = country_region_data.loc[si:]
+            except:
+                print(len(country_region_data[country_region_data['cumulative_rescaled_cases']>0]),'cases for',country_region_data['CountryName'].unique()[0])
+                continue
+            country_region_data = country_region_data.reset_index()
+
+            #Check if data
+            if len(country_region_data)<1:
+                continue
+
+            country_index = country_region_data.loc[0,'Country_index']
+            region_index = country_region_data.loc[0,'Region_index']
+            death_to_case_scale = country_region_data.loc[0,'death_to_case_scale']
+            case_death_delay = country_region_data.loc[0,'case_death_delay']
+            gross_net_income = country_region_data.loc[0,'gross_net_income']
+            population_density = country_region_data.loc[0,'population_density']
+            pdi = country_region_data.loc[0,'pdi'] #Power distance
+            idv = country_region_data.loc[0, 'idv'] #Individualism
+            mas = country_region_data.loc[0,'mas'] #Masculinity
+            uai = country_region_data.loc[0,'uai'] #Uncertainty
+            ltowvs = country_region_data.loc[0,'ltowvs'] #Long term orientation,  describes how every society has to maintain some links with its own past while dealing with the challenges of the present and future
+            ivr = country_region_data.loc[0,'ivr'] #Indulgence, Relatively weak control is called “Indulgence” and relatively strong control is called “Restraint”.
+            population = country_region_data.loc[0,'population']
+
+            if region_index!=0:
+                regions.append(country_region_data.loc[0,'CountryName']+'_'+country_region_data.loc[0,'RegionName'])
+            else:
+                regions.append(country_region_data.loc[0,'CountryName'])
+            country_region_data = country_region_data.drop(columns={'index','Country_index', 'Region_index','CountryName',
+            'RegionName','death_to_case_scale', 'case_death_delay', 'gross_net_income','population_density','pdi', 'idv', 'mas', 'uai', 'ltowvs', 'ivr','population'})
+
+            #Normalize the cases by 100'000 population
+            country_region_data['rescaled_cases']=country_region_data['rescaled_cases']#/(population/100000)
+            country_region_data['cumulative_rescaled_cases']=country_region_data['cumulative_rescaled_cases']#/(population/100000)
+
+            #Loop through and get the first 21 days of data
+            for di in range(len(country_region_data)-41):
+                #Get change over the past 21 days
+                xi = np.array(country_region_data.loc[di:di+20]).flatten()
+                change_21 = xi[-country_region_data.shape[1]:][13]-xi[:country_region_data.shape[1]][13]
+                #Add
+                X_train.append(np.append(xi,[country_index,region_index,death_to_case_scale,case_death_delay,gross_net_income,population_density,change_21,pdi, idv, mas, uai, ltowvs, ivr, population]))
+                y_train.append(np.array(country_region_data.loc[di+21:di+21+20]['rescaled_cases']))
+
+            #Get the last 3 weeks as test
+            X_test.append(X_train.pop())
+            y_test.append(y_train.pop())
+            #Save population
+            populations.append(population)
+
+    return np.array(X_train), np.array(y_train),np.array(X_test), np.array(y_test), np.array(populations), np.array(regions)
+
+class DataGenerator(keras.utils.Sequence):
+    '''Generates data for Keras'''
+    def __init__(self, X_train_fold, y_train_fold, batch_size=1, shuffle=True):
+        'Initialization'
+        self.X_train_fold = X_train_fold
+        self.y_train_fold = y_train_fold
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.X_train_fold) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        batch_indices = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        #domain_index = np.take(range((len(self.X_train_fold))),indexes)
+
+        # Generate data
+        X_batch, y_batch = self.__data_generation(batch_indices)
+
+        return X_batch, y_batch
+
+    def on_epoch_end(self): #Will be done at epoch 0 also
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.X_train_fold))
+        np.random.shuffle(self.indexes)
+
+
+    def __data_generation(self, batch_indices):
+        'Generates data containing batch_size samples'
+
+        #save data
+        #y_batch = []
+        #Generate batch_size days between 0-20 (days ahead to predict)
+        #np.random.choice(21,self.batch_size)
+        #Get the targets (y)
+        # for i in range(len(batch_indices)):
+        #     y_batch.append(self.y_train_fold[batch_indices[i],batch_days[i]])
+        #
+        # return np.append(self.X_train_fold[batch_indices],np.array([batch_days]).T,axis=-1), np.array(y_batch)
+
+        return self.X_train_fold[batch_indices],self.y_train_fold[batch_indices]
+
+#####LOSSES AND SCORES#####
+#Custom loss
+def correlationLoss(x,y, axis=-2):
+  """Loss function that maximizes the pearson correlation coefficient between the predicted values and the labels,
+  while trying to have the same mean and variance"""
+  x = tf.convert_to_tensor(x)
+  y = tf.cast(y, x.dtype)
+  n = tf.cast(tf.shape(x)[axis], x.dtype)
+  xsum = tf.reduce_sum(x, axis=axis)
+  ysum = tf.reduce_sum(y, axis=axis)
+  xmean = xsum / n
+  ymean = ysum / n
+  xsqsum = tf.reduce_sum( tf.math.squared_difference(x, xmean), axis=axis)
+  ysqsum = tf.reduce_sum( tf.math.squared_difference(y, ymean), axis=axis)
+  cov = tf.reduce_sum( (x - xmean) * (y - ymean), axis=axis)
+  corr = cov / tf.sqrt(xsqsum * ysqsum)
+  # absdif = tmean(tf.abs(x - y), axis=axis) / tf.sqrt(yvar)
+  sqdif = tf.reduce_sum(tf.math.squared_difference(x, y), axis=axis) / n / tf.sqrt(ysqsum / n)
+  # meandif = tf.abs(xmean - ymean) / tf.abs(ymean)
+  # vardif = tf.abs(xvar - yvar) / yvar
+  # return tf.convert_to_tensor( K.mean(tf.constant(1.0, dtype=x.dtype) - corr + (meandif * 0.01) + (vardif * 0.01)) , dtype=tf.float32 )
+  return tf.convert_to_tensor( K.mean(tf.constant(1.0, dtype=x.dtype) - corr + (0.01 * sqdif)) , dtype=tf.float32 )
+
+def bin_loss(y_true, y_pred):
+
+    g_loss = tf.keras.losses.mean_absolute_error(y_true, y_pred) #general, compare difference
+    kl_loss = tf.keras.losses.kullback_leibler_divergence(y_true, y_pred) #better than comparing to gaussian
+    sum_kl_loss = K.sum(kl_loss, axis =0)
+    sum_g_loss = K.sum(g_loss, axis =0)
+    #sum_g_loss = sum_g_loss*10 #This is basically a loss penalty
+    loss = sum_g_loss+sum_kl_loss
+    return loss
+
+#####BUILD NET#####
+def build_net(n1,n2,input_dim):
+    '''Build the net using Keras
+    '''
+    z = L.Input((input_dim,), name="Patient")
+    x1 = L.Dense(n1, activation="relu", name="d1")(z)
+    x2 = L.Dense(n2, activation="relu", name="d2")(x1)
+    #p1 = L.Dense(3, activation="linear", name="p1")(x3)
+    #p1 = K.abs(p1)
+    preds = L.Dense(21, activation="relu", name="preds")(x2)
+    # preds = L.Lambda(lambda x: x[0] + tf.cumsum(x[1], axis=1),
+    #                  name="preds")([p1, p2])
+    #Ensure non-negative values
+    #preds = K.abs(preds)
+    model = M.Model(z, preds, name="MQR")
+    model.compile(loss='kullback_leibler_divergence', optimizer=tf.keras.optimizers.Adam(lr=0.1, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.01, amsgrad=False),metrics=['mae','kullback_leibler_divergence'])
+    return model
+
+def test(net, X_test,y_test,populations,regions):
+    '''Test the net on the last 3 weeks of data
+    '''
+    for xi in range(X_test.shape[0]):
+        preds_i=np.absolute(net.predict(np.array([X_test[xi]]))[0])
+        R,p = pearsonr(preds_i,y_test[xi])
+        print(regions[xi],R)
+        # fig,ax = plt.subplots(figsize=(6/2.54,4/2.54))
+        # plt.plot(np.arange(1,22),y_test[xi],color='g')
+        # plt.plot(np.arange(1,22),preds_i[:,1],color='grey')
+        # plt.fill_between(np.arange(1,22),preds_i[:,0],preds_i[:,2],color='grey',alpha=0.5)
+        # plt.title(regions[xi]+'\n'+str(np.round(populations[xi]/1000000,2))+' millions')
+        # plt.tight_layout()
+        # plt.savefig(outdir+regions[xi]+'.png',dpi=300,format='png')
+        # plt.close()
+
+#####MAIN#####
+#Set font size
+matplotlib.rcParams.update({'font.size': 7})
+args = parser.parse_args()
+adjusted_data = pd.read_csv(args.adjusted_data[0],
+                 parse_dates=['Date'],
+                 encoding="ISO-8859-1",
+                 dtype={"RegionName": str,
+                        "RegionCode": str,
+                        "Country_index":int,
+                        "Region_index":int},
+                 error_bad_lines=False)
+adjusted_data = adjusted_data.fillna(0)
+outdir = args.outdir[0]
+
+#Get features
+try:
+    X_train = np.load(outdir+'X_train.npy', allow_pickle=True)
+    y_train = np.load(outdir+'y_train.npy', allow_pickle=True)
+    X_test = np.load(outdir+'X_test.npy', allow_pickle=True)
+    y_test = np.load(outdir+'y_test.npy', allow_pickle=True)
+    populations = np.load(outdir+'populations.npy', allow_pickle=True)
+    regions = np.load(outdir+'regions.npy', allow_pickle=True)
+except:
+    sel=get_features(adjusted_data)
+    X_train,y_train,X_test,y_test,populations,regions = split_for_training(sel)
+    #Save
+    np.save(outdir+'X_train.npy',X_train)
+    np.save(outdir+'y_train.npy',y_train)
+    np.save(outdir+'X_test.npy',X_test)
+    np.save(outdir+'y_test.npy',y_test)
+    np.save(outdir+'populations.npy',populations)
+    np.save(outdir+'regions.npy',regions)
+
+#Seed
+seed_everything(42) #The answer it is
+
+#Get net parameters
+BATCH_SIZE=256
+EPOCHS=100
+n1=200 #Nodes layer 1
+n2=100 #Nodes layer 2
+#Make net
+net = build_net(n1,n2,X_train.shape[1]+1)
+print(net.summary())
+#KFOLD
+NFOLD = 5
+kf = KFold(n_splits=NFOLD)
+fold=0
+
+for tr_idx, val_idx in kf.split(X_train):
+    fold+=1
+    print("FOLD", fold)
+    net = build_net(n1,n2,X_train.shape[1])
+    #Data generation
+    training_generator = DataGenerator(X_train[tr_idx], y_train[tr_idx], BATCH_SIZE)
+    valid_generator = DataGenerator(X_train[val_idx], y_train[val_idx], BATCH_SIZE)
+
+    net.fit(training_generator,
+            validation_data=valid_generator,
+            epochs=EPOCHS
+            )
+
+    #Test the net
+    test(net, X_test,y_test,populations,regions)
+    pdb.set_trace()
+pdb.set_trace()
