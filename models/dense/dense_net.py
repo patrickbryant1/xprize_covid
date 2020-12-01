26d792621038fdfb7b9fb59463847bacdfeffad4
Patrick <patrick.bryant@live.com>
2020-12-01 14:33:05 +0100

    building dense

--- models/dense/dense_net.py
+++ models/dense/dense_net.py
@@ -43,47 +43,73 @@ def seed_everything(seed=2020):
     np.random.seed(seed)
     tf.random.set_seed(seed)
 
-def get_features(adjusted_data):
+def get_features(adjusted_data, train_days, outdir):
     '''Get the selected features
     '''
 
-    selected_features = ['C1_School closing',
-                        'C2_Workplace closing',
-                        'C3_Cancel public events',
-                        'C4_Restrictions on gatherings',
-                        'C5_Close public transport',
-                        'C6_Stay at home requirements',
-                        'C7_Restrictions on internal movement',
-                        'C8_International travel controls',
-                        'H1_Public information campaigns',
-                        'H2_Testing policy',
-                        'H3_Contact tracing',
-                        'H6_Facial Coverings', #These first 12 are the ones the prescriptor will assign
-                        'Country_index',
-                        'Region_index',
-                        'CountryName',
-                        'RegionName',
-                        'rescaled_cases',
-                        'cumulative_rescaled_cases',
-                        'death_to_case_scale',
-                        'case_death_delay',
-                        'gross_net_income',
-                        'population_density',
-                        'monthly_temperature',
-                        'retail_and_recreation',
-                        'grocery_and_pharmacy',
-                        'parks',
-                        'transit_stations',
-                        'workplaces',
-                        'residential',
-                        'pdi', 'idv', 'mas', 'uai', 'ltowvs', 'ivr',
-                        'population']
 
-    sel = adjusted_data[selected_features]
 
-    return sel
+    #Get features
+    try:
+        X_train = np.load(outdir+'X_train.npy', allow_pickle=True)
+        y_train = np.load(outdir+'y_train.npy', allow_pickle=True)
+        X_test = np.load(outdir+'X_test.npy', allow_pickle=True)
+        y_test = np.load(outdir+'y_test.npy', allow_pickle=True)
+        populations = np.load(outdir+'populations.npy', allow_pickle=True)
+        regions = np.load(outdir+'regions.npy', allow_pickle=True)
 
-def split_for_training(sel):
+    except:
+        selected_features = ['C1_School closing',
+                            'C2_Workplace closing',
+                            'C3_Cancel public events',
+                            'C4_Restrictions on gatherings',
+                            'C5_Close public transport',
+                            'C6_Stay at home requirements',
+                            'C7_Restrictions on internal movement',
+                            'C8_International travel controls',
+                            'H1_Public information campaigns',
+                            'H2_Testing policy',
+                            'H3_Contact tracing',
+                            'H6_Facial Coverings', #These first 12 are the ones the prescriptor will assign
+                            'Country_index',
+                            'Region_index',
+                            'CountryName',
+                            'RegionName',
+                            'smoothed_cases',
+                            'cumulative_smoothed_cases',
+                            'rescaled_cases',
+                            'cumulative_rescaled_cases',
+                            'death_to_case_scale',
+                            'case_death_delay',
+                            'gross_net_income',
+                            'population_density',
+                            'monthly_temperature',
+                            'retail_and_recreation',
+                            'grocery_and_pharmacy',
+                            'parks',
+                            'transit_stations',
+                            'workplaces',
+                            'residential',
+                            'pdi', 'idv', 'mas', 'uai', 'ltowvs', 'ivr',
+                            'population']
+
+        sel = adjusted_data[selected_features]
+
+        X_train,y_train,X_test,y_test,populations,regions = split_for_training(sel,train_days)
+        #Save
+        np.save(outdir+'X_train.npy',X_train)
+        np.save(outdir+'y_train.npy',y_train)
+        np.save(outdir+'X_test.npy',X_test)
+        np.save(outdir+'y_test.npy',y_test)
+        np.save(outdir+'populations.npy',populations)
+        np.save(outdir+'regions.npy',regions)
+
+
+    return X_train,y_train,X_test,y_test,populations,regions
+
+
+
+def split_for_training(sel, train_days):
     '''Split the data for training and testing
     '''
     X_train = [] #Inputs
@@ -106,6 +132,7 @@ def split_for_training(sel):
             except:
                 print(len(country_region_data[country_region_data['cumulative_rescaled_cases']>0]),'cases for',country_region_data['CountryName'].unique()[0])
                 continue
+
             country_region_data = country_region_data.reset_index()
 
             #Check if data
@@ -125,26 +152,30 @@ def split_for_training(sel):
             ltowvs = country_region_data.loc[0,'ltowvs'] #Long term orientation,  describes how every society has to maintain some links with its own past while dealing with the challenges of the present and future
             ivr = country_region_data.loc[0,'ivr'] #Indulgence, Relatively weak control is called “Indulgence” and relatively strong control is called “Restraint”.
             population = country_region_data.loc[0,'population']
-
             if region_index!=0:
                 regions.append(country_region_data.loc[0,'CountryName']+'_'+country_region_data.loc[0,'RegionName'])
             else:
                 regions.append(country_region_data.loc[0,'CountryName'])
+
             country_region_data = country_region_data.drop(columns={'index','Country_index', 'Region_index','CountryName',
-            'RegionName','death_to_case_scale', 'case_death_delay', 'gross_net_income','population_density','pdi', 'idv', 'mas', 'uai', 'ltowvs', 'ivr','population'})
+            'RegionName', 'death_to_case_scale', 'case_death_delay', 'gross_net_income','population_density','pdi', 'idv',
+             'mas', 'uai', 'ltowvs', 'ivr',
+             'rescaled_cases','cumulative_rescaled_cases',
+             'population'})
 
             #Normalize the cases by 100'000 population
-            country_region_data['rescaled_cases']=country_region_data['rescaled_cases']#/(population/100000)
-            country_region_data['cumulative_rescaled_cases']=country_region_data['cumulative_rescaled_cases']#/(population/100000)
+            country_region_data['smoothed_cases']=country_region_data['smoothed_cases']/(population/100000)
+            country_region_data['cumulative_smoothed_cases']=country_region_data['cumulative_smoothed_cases']/(population/100000)
 
-            #Loop through and get the first 21 days of data
-            for di in range(len(country_region_data)-41):
+            #Loop through and get the data
+            forecast_days=21
+            for di in range(len(country_region_data)-(train_days+forecast_days-1)):
                 #Get change over the past 21 days
-                xi = np.array(country_region_data.loc[di:di+20]).flatten()
+                xi = np.array(country_region_data.loc[di:di+train_days-1]).flatten()
                 change_21 = xi[-country_region_data.shape[1]:][13]-xi[:country_region_data.shape[1]][13]
                 #Add
                 X_train.append(np.append(xi,[country_index,region_index,death_to_case_scale,case_death_delay,gross_net_income,population_density,change_21,pdi, idv, mas, uai, ltowvs, ivr, population]))
-                y_train.append(np.array(country_region_data.loc[di+21:di+21+20]['rescaled_cases']))
+                y_train.append(np.array(country_region_data.loc[di+train_days:di++train_days+forecast_days-1]['smoothed_cases']))
 
             #Get the last 3 weeks as test
             X_test.append(X_train.pop())
@@ -153,6 +184,7 @@ def split_for_training(sel):
             populations.append(population)
 
     return np.array(X_train), np.array(y_train),np.array(X_test), np.array(y_test), np.array(populations), np.array(regions)
+
 
 class DataGenerator(keras.utils.Sequence):
     '''Generates data for Keras'''
@@ -280,30 +312,20 @@ adjusted_data = pd.read_csv(args.adjusted_data[0],
                         "Region_index":int},
                  error_bad_lines=False)
 adjusted_data = adjusted_data.fillna(0)
+days_ahead = args.days_ahead[0]
+start_date = args.start_date[0]
+train_days = args.train_days[0]
 outdir = args.outdir[0]
-
-#Get features
-try:
-    X_train = np.load(outdir+'X_train.npy', allow_pickle=True)
-    y_train = np.load(outdir+'y_train.npy', allow_pickle=True)
-    X_test = np.load(outdir+'X_test.npy', allow_pickle=True)
-    y_test = np.load(outdir+'y_test.npy', allow_pickle=True)
-    populations = np.load(outdir+'populations.npy', allow_pickle=True)
-    regions = np.load(outdir+'regions.npy', allow_pickle=True)
-except:
-    sel=get_features(adjusted_data)
-    X_train,y_train,X_test,y_test,populations,regions = split_for_training(sel)
-    #Save
-    np.save(outdir+'X_train.npy',X_train)
-    np.save(outdir+'y_train.npy',y_train)
-    np.save(outdir+'X_test.npy',X_test)
-    np.save(outdir+'y_test.npy',y_test)
-    np.save(outdir+'populations.npy',populations)
-    np.save(outdir+'regions.npy',regions)
 
 #Seed
 seed_everything(42) #The answer it is
+#Use only data from start date
+adjusted_data = adjusted_data[adjusted_data['Date']>=start_date]
+#Get features
+X_train,y_train,X_test,y_test,populations,regions  = get_features(adjusted_data,train_days,outdir)
 
+
+pdb.set_trace()
 #Get net parameters
 BATCH_SIZE=256
 EPOCHS=100
