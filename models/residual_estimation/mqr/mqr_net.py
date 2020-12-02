e4e375fe4fe8fd93e59291bb4df8af909289d83d
Patrick <patrick.bryant@live.com>
2020-12-02 11:17:37 +0100

    fitting residuals

--- models/residual_estimation/mqr/mqr_net.py
+++ models/residual_estimation/mqr/mqr_net.py
@@ -18,7 +18,7 @@ from tensorflow import keras
 import tensorflow.keras.backend as K
 import tensorflow.keras.layers as L
 import tensorflow.keras.models as M
-
+from tensorflow.keras.callbacks import TensorBoard
 from scipy.stats import pearsonr
 
 import pdb
@@ -27,11 +27,11 @@ parser = argparse.ArgumentParser(description = '''A multiple Quantile regression
 parser.add_argument('--days_ahead', nargs=1, type= int,
                   default=sys.stdin, help = 'Number of days ahead to fit')
 parser.add_argument('--X_train', nargs=1, type= str,
-                  default=sys.stdin, help = 'Path to predictions of train data.')
+                  default=sys.stdin, help = 'Path to train data.')
 parser.add_argument('--train_preds', nargs=1, type= str,
                   default=sys.stdin, help = 'Path to predictions of train data.')
 parser.add_argument('--residuals', nargs=1, type= str,
-                  default=sys.stdin, help = 'Path to true values of train data.')
+                  default=sys.stdin, help = 'Path to residuals for true values of train data.')
 #parser.add_argument('--param_combo', nargs=1, type= int, default=sys.stdin, help = 'Parameter combo.')
 parser.add_argument('--outdir', nargs=1, type= str,
                   default=sys.stdin, help = 'Path to output directory. Include /in end')
@@ -80,24 +80,6 @@ class DataGenerator(keras.utils.Sequence):
         return self.X_train_fold[batch_indices],self.y_train_fold[batch_indices]
 
 #####LOSSES AND SCORES#####
-#####LOSSES AND SCORES#####
-def test(net, X_test,y_test,populations,regions):
-    '''Test the net on the last 3 weeks of data
-    '''
-
-    test_preds=net.predict(np.array(X_test))
-    R,p = pearsonr(test_preds[:,1],y_test)
-    print('PCC:',R)
-    order = np.argsort(y_test)
-    plt.plot(y_test[order],test_preds[:,1][order],color='grey')
-    plt.plot(y_test[order],y_test[order],color='k',linestyle='--')
-    plt.fill_between(y_test[order],test_preds[:,0][order],test_preds[:,2][order],color='grey',alpha=0.5)
-    plt.xlim([min(y_test),max(y_test)])
-    plt.ylim([min(y_test),max(y_test)])
-    plt.xlabel('True')
-    plt.ylabel('Pred')
-    plt.show()
-
 #Custom loss
 #============================#
 def qloss(y_true, y_pred):
@@ -129,7 +111,22 @@ def build_net(n1,n2,input_dim):
     model.compile(loss=qloss, optimizer=tf.keras.optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.01, amsgrad=False),metrics=['mae'])
     return model
 
+def test(net, X_test,y_test):
+    '''Test the net on the last 3 weeks of data
+    '''
 
+    test_preds=net.predict(np.array(X_test))
+    R,p = pearsonr(test_preds[:,1],y_test)
+    print('PCC:',R)
+    order = np.argsort(y_test)
+    plt.plot(y_test[order],test_preds[:,1][order],color='grey')
+    plt.plot(y_test[order],y_test[order],color='k',linestyle='--')
+    plt.fill_between(y_test[order],test_preds[:,0][order],test_preds[:,2][order],color='grey',alpha=0.5)
+    plt.xlim([min(y_test),max(y_test)])
+    plt.ylim([min(y_test),max(y_test)])
+    plt.xlabel('True')
+    plt.ylabel('Pred')
+    plt.show()
 #####MAIN#####
 #Set font size
 matplotlib.rcParams.update({'font.size': 7})
@@ -140,12 +137,16 @@ train_preds = np.load(args.train_preds[0],allow_pickle=True)
 residuals = np.load(args.residuals[0],allow_pickle=True)
 outdir = args.outdir[0]
 
+#Append pred to X_train
+X_train = np.append(X_train,np.array([train_preds[days_ahead-1,:]]).T,axis=1)
+#Get redisuals for day
+y_train = residuals[days_ahead-1,:]
 #Seed
 seed_everything(42) #The answer it is
 
 #Get net parameters
-BATCH_SIZE=256
-EPOCHS=200
+BATCH_SIZE=128
+EPOCHS=50
 n1=16 #Nodes layer 1
 n2=16 #Nodes layer 2
 
@@ -156,12 +157,12 @@ print(net.summary())
 NFOLD = 5
 kf = KFold(n_splits=NFOLD)
 fold=0
-pdb.set_trace()
+
 for tr_idx, val_idx in kf.split(X_train):
     fold+=1
     tensorboard = TensorBoard(log_dir=outdir+'fold'+str(fold))
     print("FOLD", fold)
-    net = build_net(n1,n2,X_train.shape[1],bins)
+    net = build_net(n1,n2,X_train.shape[1])
     #Data generation
     training_generator = DataGenerator(X_train[tr_idx], y_train[tr_idx], BATCH_SIZE)
     valid_generator = DataGenerator(X_train[val_idx], y_train[val_idx], BATCH_SIZE)
@@ -173,6 +174,7 @@ for tr_idx, val_idx in kf.split(X_train):
             )
 
     #Test the net
-    test(net, X_test,y_test,populations,regions)
+    random_selection = np.random.choice(X_train.shape[0],100)
+    test(net, X_train[random_selection],y_train[random_selection])
     pdb.set_trace()
 pdb.set_trace()
--- models/residual_estimation/mqr/train.sh
+++ models/residual_estimation/mqr/train.sh
@@ -3,5 +3,5 @@ DAYS_AHEAD=21
 XTRAIN=/home/patrick/results/COVID19/xprize/theil_sen/per100000/X_train.npy
 TRAIN_PREDS=/home/patrick/results/COVID19/xprize/theil_sen/per100000/residuals/train_preds.npy
 RESIDUALS=/home/patrick/results/COVID19/xprize/theil_sen/per100000/residuals/residuals.npy
-OUTDIR=/home/patrick/results/COVID19/xprize/theil_sen/per100000/residuals/MQR
+OUTDIR=/home/patrick/results/COVID19/xprize/theil_sen/per100000/residuals/MQR/
 ./mqr_net.py --days_ahead $DAYS_AHEAD --X_train $XTRAIN --train_preds $TRAIN_PREDS --residuals $RESIDUALS --outdir $OUTDIR
