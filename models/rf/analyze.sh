fdbed8ed1d73d6a72139cd1a12b907f82343bb5b
Patrick <patrick.bryant@live.com>
2020-12-10 16:44:15 +0100

    looking at feature importances

--- models/rf/analyze.sh
+++ models/rf/analyze.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 INDIR=/home/patrick/results/COVID19/xprize/simple_rf/comparing_median/high/
 OUTDIR=/home/patrick/results/COVID19/xprize/simple_rf/comparing_median/high/
-./analyze_avg.py --indir $INDIR --outdir $OUTDIR
+./analyze.py --indir $INDIR --outdir $OUTDIR
 
 INDIR=/home/patrick/results/COVID19/xprize/simple_rf/comparing_median/low/
 OUTDIR=/home/patrick/results/COVID19/xprize/simple_rf/comparing_median/low/
-./analyze_avg.py --indir $INDIR --outdir $OUTDIR
+./analyze.py --indir $INDIR --outdir $OUTDIR
