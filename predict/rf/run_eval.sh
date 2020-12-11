b299a8dae7509ce6a83acea2d0d6228a757c2b81
Patrick <patrick.bryant@live.com>
2020-12-11 13:45:22 +0100

    plotting

--- predict/rf/run_eval.sh
+++ predict/rf/run_eval.sh
@@ -3,6 +3,6 @@
 #March
 #../evaluate.py --in_csv ./all_predictions_2020-03-07_2020-03-31.csv --outdir ./
 #June
-../evaluate.py --in_csv ./all_predictions_2020-06-07_2020-06-31.csv --outdir ./
+#../evaluate.py --in_csv ./all_predictions_2020-06-07_2020-06-31.csv --outdir ./
 #November
-../evaluate.py --in_csv ./all_predictions_2020-11-07_2020-11-31.csv --outdir ./
+../evaluate.py --in_csv ./all_predictions_2020-11-01_2020-11-30.csv --outdir ./
