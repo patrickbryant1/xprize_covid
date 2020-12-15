7252de5805afce02ab79986cb235832d97148431
Patrick <patrick.bryant@live.com>
2020-12-15 22:22:32 +0100

    building iterative net

--- models/iterative/iter_net.py
+++ models/iterative/iter_net.py
@@ -102,27 +102,26 @@ def get_features(adjusted_data,train_days,forecast_days,num_pred_periods,t,outdi
         X1,X2,X3,y = split_for_training(sel,train_days,forecast_days,num_pred_periods)
 
         #Save
-        np.save(outdir+'X1.npy',X)
-        np.save(outdir+'X2.npy',X)
-        np.save(outdir+'X3.npy',X)
+        np.save(outdir+'X1.npy',X1)
+        np.save(outdir+'X2.npy',X2)
+        np.save(outdir+'X3.npy',X3)
         np.save(outdir+'y.npy',y)
 
     #Split into high and low
-    high_i = np.argwhere(X[:,13]>t)
-    low_i = np.argwhere(X[:,13]<=t)
+    high_i = np.argwhere(X1[:,13]>t)
+    low_i = np.argwhere(X1[:,13]<=t)
     X1_high = X1[high_i][:,0,:]
     X1_low = X1[low_i][:,0,:]
     X2_high = X2[high_i][:,0,:]
     X2_low = X2[low_i][:,0,:]
     X3_high = X3[high_i][:,0,:]
-    X4_low = X3[low_i][:,0,:]
+    X3_low = X3[low_i][:,0,:]
 
     y_high = y[high_i][:,0]
     y_low = y[low_i][:,0]
 
-    pdb.set_trace()
 
-    return X_high,y_high,X_low,y_low
+    return X1_high,X1_low,X2_high,X2_low,X3_high,X3_low,y_high,y_low
 
 
 def split_for_training(sel,train_days,forecast_days,num_pred_periods):
@@ -190,13 +189,14 @@ def split_for_training(sel,train_days,forecast_days,num_pred_periods):
             'RegionName', 'death_to_case_scale', 'case_death_delay', 'gross_net_income','population_density','pdi', 'idv',
              'mas', 'uai', 'ltowvs', 'ivr','Urban population (% of total population)','Population ages 65 and above (% of total population)',
              'GDP per capita (current US$)', 'Obesity Rate (%)', 'Cancer Rate (%)', 'Share of Deaths from Smoking (%)', 'Pneumonia Death Rate (per 100K)',
-             'Share of Deaths from Air Pollution (%)','CO2 emissions (metric tons per capita)', 'Air transport (# carrier departures worldwide)','population'})
+             'Share of Deaths from Air Pollution (%)','CO2 emissions (metric tons per capita)', 'Air transport (# carrier departures worldwide)','population',
+             'cumulative_smoothed_cases'})
 
             #Normalize the cases by 100'000 population
             #country_region_data['rescaled_cases']=country_region_data['rescaled_cases']/(population/_high000)
             #country_region_data['cumulative_rescaled_cases']=country_region_data['cumulative_rescaled_cases']/(population/_high000)
             country_region_data['smoothed_cases']=country_region_data['smoothed_cases']/(population/100000)
-            country_region_data['cumulative_smoothed_cases']=country_region_data['cumulative_smoothed_cases']/(population/100000)
+            #country_region_data['cumulative_smoothed_cases']=country_region_data['cumulative_smoothed_cases']/(population/100000)
             #Loop through and get the data
 
             for di in range(len(country_region_data)-(train_days*num_pred_periods+forecast_days-1)):
@@ -266,9 +266,11 @@ def kfold(num_regions, NFOLD):
 
 class DataGenerator(keras.utils.Sequence):
     '''Generates data for Keras'''
-    def __init__(self, X_train_fold, y_train_fold, batch_size=1, shuffle=True):
+    def __init__(self, X1_train_fold,X2_train_fold,X3_train_fold, y_train_fold, batch_size=1, shuffle=True):
         'Initialization'
-        self.X_train_fold = X_train_fold
+        self.X1_train_fold = X1_train_fold
+        self.X2_train_fold = X2_train_fold
+        self.X3_train_fold = X3_train_fold
         self.y_train_fold = y_train_fold
         self.batch_size = batch_size
         self.shuffle = shuffle
@@ -285,20 +287,20 @@ class DataGenerator(keras.utils.Sequence):
         #domain_index = np.take(range((len(self.X_train_fold))),indexes)
 
         # Generate data
-        X_batch, y_batch = self.__data_generation(batch_indices)
-
-        return X_batch, y_batch
+        X1_batch, X2_batch, X3_batch, y_batch = self.__data_generation(batch_indices)
+        pdb.set_trace()
+        return [X1_batch, X2_batch, X3_batch], y_batch
 
     def on_epoch_end(self): #Will be done at epoch 0 also
         'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.X_train_fold))
+        self.indexes = np.arange(len(self.X1_train_fold))
         np.random.shuffle(self.indexes)
 
 
     def __data_generation(self, batch_indices):
         'Generates data containing batch_size samples'
 
-        return self.X_train_fold[batch_indices],self.y_train_fold[batch_indices]
+        return self.X1_train_fold[batch_indices],self.X2_train_fold[batch_indices],self.X3_train_fold[batch_indices],self.y_train_fold[batch_indices]
 
 #####LOSSES AND SCORES#####
 
@@ -306,35 +308,38 @@ class DataGenerator(keras.utils.Sequence):
 #============================#
 
 #####BUILD NET#####
-def build_net(n1,n2,input_dim):
+def build_net(n1,n2,dim1,dim2,dim3):
     '''Build the net using Keras
     '''
     shared_dense1 = L.Dense(n1, activation="relu", name="d1")
-    shared_dense2 = L.Dense(n2, activation="relu", name="d2")
+    shared_dense2 = L.Dense(1, activation="relu", name="d2")
 
-    inp1 = L.Input((input_dim,), name="inp1") #Inputs with median cases
-    inp2 = L.Input((input_dim,), name="inp2") #Inputs without median cases
-    inp3 = L.Input((input_dim,), name="inp3") #Inputs without median cases
+    inp1 = L.Input((dim1,), name="inp1") #Inputs with median cases
+    inp2 = L.Input((dim2,), name="inp2") #Inputs without median cases
+    inp3 = L.Input((dim3,), name="inp3") #Inputs without median cases
 
     pred_step1 = shared_dense2(shared_dense1(inp1))
     #Concat preds 1 with inp 2
-    pred_step1 = shared_dense2(shared_dense1(inp2))
+    pred_in2 = L.Concatenate(axis=1)([inp2,pred_step1])
+    pred_step2 = shared_dense2(shared_dense1(pred_in2))
+    #Concat preds 2 with inp 3
+    pred_in3 = L.Concatenate()([inp3,pred_step2])
+    pred_step3 = shared_dense2(shared_dense1(pred_in3))
 
+    preds = L.Concatenate()([pred_step1,pred_step2,pred_step3])
 
-    model = M.Model(z, preds, name="Dense")
+    model = M.Model([in1,in2,in3], preds, name="Dense")
     model.compile(loss='mae', optimizer=tf.keras.optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.01, amsgrad=False),metrics=['mae'])
     return model
 
-def fit_data(X,y,mode,outdir):
+def fit_data(X1,X2,X3,y,mode,outdir):
 
     #Get inpu2 - no cases
 
-
-    pdb.set_trace()
     #KFOLD
     NFOLD = 5
     #kf = KFold(n_splits=NFOLD,shuffle=True, random_state=42)
-    train_split, val_split = kfold(len(X),NFOLD)
+    train_split, val_split = kfold(len(X1),NFOLD)
     #Save errors
     errors = []
     corrs = []
@@ -342,11 +347,11 @@ def fit_data(X,y,mode,outdir):
     #Get net parameters
     BATCH_SIZE=64
     EPOCHS=100
-    n1=X.shape[1] #Nodes layer 1
-    n2=X.shape[1] #Nodes layer 2
+    n1=X1.shape[1] #Nodes layer 1
+    n2=X1.shape[1] #Nodes layer 2
 
     #Make net
-    net = build_net(n1,n2,X.shape[1])
+    net = build_net(n1,n2,X1.shape[1],X2.shape[1],X3.shape[1])
     print(net.summary())
 
     for fold in range(NFOLD):
@@ -356,8 +361,8 @@ def fit_data(X,y,mode,outdir):
         print("FOLD", fold)
         net = build_net(n1,n2,X.shape[1])
         #Data generation
-        training_generator = DataGenerator(X[tr_idx], y[tr_idx], BATCH_SIZE)
-        valid_generator = DataGenerator(X[val_idx], y[val_idx], BATCH_SIZE)
+        training_generator = DataGenerator(X1[tr_idx], X2[tr_idx], X3[tr_idx], y[tr_idx], BATCH_SIZE)
+        valid_generator = DataGenerator(X1[val_idx], X2[val_idx], X3[val_idx], y[val_idx], BATCH_SIZE)
         pdb.set_trace()
         net.fit(training_generator,
                 validation_data=valid_generator,
@@ -373,8 +378,6 @@ def fit_data(X,y,mode,outdir):
 
 #####MAIN#####
 args = parser.parse_args()
-#Seed
-seed_everything(0) #The answer it is
 adjusted_data = pd.read_csv(args.adjusted_data[0],
                  parse_dates=['Date'],
                  encoding="ISO-8859-1",
@@ -393,14 +396,12 @@ outdir = args.outdir[0]
 #Use only data from start date
 adjusted_data = adjusted_data[adjusted_data['Date']>=start_date]
 #Get data
-X_high,y_high,X_low,y_low =  get_features(adjusted_data,train_days,forecast_days,num_pred_periods,threshold,outdir)
-
-pdb.set_trace()
+X1_high,X1_low,X2_high,X2_low,X3_high,X3_low,y_high,y_low = get_features(adjusted_data,train_days,forecast_days,num_pred_periods,threshold,outdir)
 
 
 #Fit high
 #Convert to log for training
-fit_data(X_high,np.log(y_high+0.001),'high',outdir)
+fit_data(X1_high,X2_high,X3_high,np.log(y_high+0.001),'high',outdir)
 
 pdb.set_trace()
 pdb.set_trace()
