28df72c9697aa18de0beb33ccf2eeef095bd527d
Patrick Bryant <>
2021-01-25 08:36:01 +0100

    setting up pretrained prescriptor

--- prescribe/run_trained/prescribe.py
+++ prescribe/run_trained/prescribe.py
@@ -48,6 +48,43 @@ parser.add_argument('--outdir', nargs=1, type= str,
                   default=sys.stdin, help = 'Path to output directory. Include /in end')
 
 ###FUNCTIONS###
+def prescribe(start_date_str, end_date_str, path_to_prior_ips_file, path_to_cost_file):
+    '''Prescribe using the pretrained prescriptor
+    '''
+
+    start_date = pd.to_datetime(start_date_str, format='%Y-%m-%d')
+    end_date = pd.to_datetime(end_date_str, format='%Y-%m-%d')
+
+    # Load the past IPs data
+    print("Loading past IPs data...")
+    past_ips_df = load_ips_file(path_to_prior_ips_file)
+    geos = past_ips_df['GeoID'].unique()
+
+    # Load historical data with basic preprocessing
+    print("Loading historical data...")
+    df = prepare_historical_df()
+
+    # Restrict it to dates before the start_date
+    df = df[df['Date'] <= start_date]
+
+ip_costs = pd.read_csv(args.ip_costs[0])
+ip_costs['GeoID'] = np.where(ip_costs["RegionName"].isnull(),
+                              ip_costs["CountryName"],
+                              ip_costs["CountryName"] + ' / ' + ip_costs["RegionName"])
+start_date = args.start_date[0]
+lookback_days = args.lookback_days[0]
+forecast_days = args.forecast_days[0]
+outdir = args.outdir[0]
+#Load the model input data
+load_inp_data(start_date, lookback_days,ip_costs)
+#Load model for case prediction
+load_model()
+
+#Save
+np.save(outdir+'population.npy',np.array(pop))
+np.save(outdir+'front.npy',front)
+
+
 # Construct model
 class Positive(Constraint):
 
@@ -223,66 +260,6 @@ def load_inp_data(start_date,lookback_days,ip_costs):
     return None
 
 
-def setup_nsga3(NOBJ, NDIM, P, BOUND_LOW, BOUND_UP, CXPB, MUTPB, start_date, lookback_days,ip_costs):
-    #https://deap.readthedocs.io/en/master/examples/nsga3.html
-    #Problem definition
-    '''
-    #Number of models in the pareto front
-    '''
-    H = 100 #factorial(NOBJ + P - 1) / (factorial(P) * factorial(NOBJ - 1))
-
-
-    '''
-    Then we define the various parameters for the algorithm, including the population size set
-    to the first multiple of 4 greater than H, the number of generations and variation probabilities.
-    If twelve divisions (P = 12) are considered for each objective axis and there are 3 objectives,
-    91 reference points will be created, according to equation (10).
-    (3+12-1) choose 12 = 14 choose 12 = 14!/[(14-12)!12!]=14*13/2=91
-    '''
-    MU = int(H + (4 - H % 4))
-
-
-    '''
-    Next, NSGA-III selection requires a reference point set. The reference point set serves to
-    guide the evolution into creating a uniform Pareto front in the objective space.
-    '''
-    ref_points = tools.uniform_reference_points(NOBJ, P)
-
-    '''
-    As in any DEAP program, we need to populate the creator with the type of individual we require for our optimization.
-    In this case we will use a basic list genotype and minimization fitness.
-    '''
-    creator.create("FitnessMin", base.Fitness, weights=(-1.0,) * NOBJ)
-    creator.create("Individual", list, fitness=creator.FitnessMin)
-
-    '''
-    Moreover, we need to populate the evolutionary toolbox with initialization, variation and selection operators.
-    Note how we provide the reference point set to the NSGA-III selection scheme.
-    '''
-
-    def uniform(low, up, size=None):
-        try:
-            return [random.uniform(a, b) for a, b in zip(low, up)]
-        except TypeError:
-            return [random.uniform(a, b) for a, b in zip([low] * size, [up] * size)]
-
-    toolbox = base.Toolbox()
-    toolbox.register("attr_float", uniform, BOUND_LOW, BOUND_UP, NDIM) #12 interventions
-    toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.attr_float)
-    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
-    #Register the evaluation, mating, mutation and selection processes
-    #Load models for evaluation
-    load_model()
-    toolbox.register("evaluate", evaluate_npis)
-    #eta = Crowding degree of the crossover.
-    #A high eta will produce children resembling to their parents, while a small eta will produce solutions much more different.
-    toolbox.register("mate", tools.cxSimulatedBinaryBounded, low=BOUND_LOW, up=BOUND_UP, eta=30.0)
-    #indpb – Independent probability for each attribute to be mutated.
-    toolbox.register("mutate", tools.mutPolynomialBounded, low=BOUND_LOW, up=BOUND_UP, eta=20.0, indpb=1.0/NDIM)
-    toolbox.register("select", tools.selNSGA3, ref_points=ref_points)
-
-    return toolbox, creator, MU
 
 
 def convert_ratios_to_total_cases(ratios, window_size, prev_new_cases, initial_total_cases,pop_sizes):
@@ -406,110 +383,38 @@ def evaluate_npis(individual):
     return obj1, obj2
 
 
-
-def train(seed,toolbox, creator,NGEN, CXPB, MUTPB):
-    '''
-    The main part of the evolution is mostly similar to any other DEAP example.
-    The algorithm used is close to the eaSimple() algorithm as crossover and mutation are applied to
-    every individual (see variation probabilities above). However, the selection is made from the
-    parent and offspring populations instead of completely replacing the parents with the offspring.
-    '''
-
-    random.seed(seed)
-
-    # Initialize statistics object
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", numpy.mean, axis=0)
-    stats.register("std", numpy.std, axis=0)
-    stats.register("min", numpy.min, axis=0)
-    stats.register("max", numpy.max, axis=0)
-
-    logbook = tools.Logbook()
-    logbook.header = "gen", "evals", "std", "min", "avg", "max"
-
-    pop = toolbox.population(n=MU)
-
-    # Evaluate the individuals with an invalid fitness
-    invalid_ind = [ind for ind in pop if not ind.fitness.valid]
-    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
-    for ind, fit in zip(invalid_ind, fitnesses):
-        ind.fitness.values = fit
-
-    # Compile statistics about the population
-    record = stats.compile(pop)
-    logbook.record(gen=0, evals=len(invalid_ind), **record)
-    print(logbook.stream)
-
-    # Begin the generational process
-    for gen in range(1, NGEN):
-        offspring = algorithms.varAnd(pop, toolbox, CXPB, MUTPB)
-
-        # Evaluate the individuals with an invalid fitness
-        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
-        for ind, fit in zip(invalid_ind, fitnesses):
-            ind.fitness.values = fit
-
-        # Select the next generation population from parents and offspring
-        pop = toolbox.select(pop + offspring, MU)
-
-        # Compile statistics about the new population
-        record = stats.compile(pop)
-        logbook.record(gen=gen, evals=len(invalid_ind), **record)
-        print(logbook.stream)
-
-
-    return pop, logbook
-
-
 ##########MAIN###########
 
-#Params for NSGA3
-#Parse args
-args = parser.parse_args()
-
-ip_costs = pd.read_csv(args.ip_costs[0])
-ip_costs['GeoID'] = np.where(ip_costs["RegionName"].isnull(),
-                              ip_costs["CountryName"],
-                              ip_costs["CountryName"] + ' / ' + ip_costs["RegionName"])
-start_date = args.start_date[0]
-lookback_days = args.lookback_days[0]
-forecast_days = args.forecast_days[0]
-outdir = args.outdir[0]
-#Load the model input data
-load_inp_data(start_date, lookback_days,ip_costs)
-
-#Create the prescriptor setup
-NOBJ = 2
-NUM_WEIGHTS=2
-NUM_LAYERS=2
-NDIM = 12*NUM_WEIGHTS*NUM_LAYERS #Number of dimensions for net (prescriptor): 12 ips, 2 weights (1 for the ip, 1 for the cases), 2 layeres
-P = 12  #Number of divisions considered for each objective axis
-#If P divisions are considered for each objective axis, and
-#there are N objective axes, (N+P-1) choose P reference points Will
-#be created = (N+P-1)!/((N+P-1-P)!P!)
-#If N=2 and P= 12 --> 13!/(12!*1!)=13
-
-#Weight boundaries
-BOUND_LOW, BOUND_UP = 0.0, 1.0
-NGEN = 200 #Number of generations to run
-CXPB = 1.0 #The probability of mating two individuals.
-MUTPB = 1.0 #The probability of mutating an individual.
-
-toolbox, creator, MU = setup_nsga3(NOBJ, NDIM, P, BOUND_LOW, BOUND_UP, CXPB, MUTPB,start_date,lookback_days,ip_costs)
-
-pop, logbook = train(42,toolbox, creator,NGEN, CXPB, MUTPB)
-
-#Get the pareto front
-front = numpy.array([ind.fitness.values for ind in pop])
-#Save
-np.save(outdir+'population.npy',np.array(pop))
-np.save(outdir+'front.npy',front)
-#Plot
-plt.scatter(front[:,0], front[:,1], c="b")
-plt.xlabel('Cases')
-plt.ylabel('Stringency')
-plt.title('Pareto front')
-plt.axis("tight")
-plt.savefig(outdir+'pareto_front.png',format='png')
-pdb.set_trace()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--start_date",
+                        dest="start_date",
+                        type=str,
+                        required=True,
+                        help="Start date from which to prescribe, included, as YYYY-MM-DD."
+                             "For example 2020-08-01")
+    parser.add_argument("-e", "--end_date",
+                        dest="end_date",
+                        type=str,
+                        required=True,
+                        help="End date for the last prescription, included, as YYYY-MM-DD."
+                             "For example 2020-08-31")
+    parser.add_argument("-ip", "--interventions_past",
+                        dest="prev_file",
+                        type=str,
+                        required=True,
+                        help="The path to a .csv file of previous intervention plans")
+    parser.add_argument("-c", "--intervention_costs",
+                        dest="cost_file",
+                        type=str,
+                        required=True,
+                        help="Path to a .csv file containing the cost of each IP for each geo")
+    parser.add_argument("-o", "--output_file",
+                        dest="output_file",
+                        type=str,
+                        required=True,
+                        help="The path to an intervention plan .csv file")
+    args = parser.parse_args()
+    print(f"Generating prescriptions from {args.start_date} to {args.end_date}...")
+    prescribe(args.start_date, args.end_date, args.prev_file, args.cost_file, args.output_file)
+    print("Done!")
