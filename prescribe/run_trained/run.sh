d25efb44a7d8925cf5b60e4536a3cfe5bc4b2780
patrick.bryant1 <>
2021-02-22 15:58:49 +0100

    Update

--- prescribe/run_trained/run.sh
+++ prescribe/run_trained/run.sh
@@ -4,5 +4,5 @@ START_DATE='2020-02-15' #Date to start from
 END_DATE='2021-05-15' #Date to end
 HISTIP=../../data/historical_ip.csv
 IPCOSTS=../../data/uniform_costs.csv
-OUTFILE=prescr_20201201_20210101.csv
+OUTFILE=prescr_20210215_20210501.csv
 time python3 prescribe.py -s $START_DATE -e $END_DATE -ip $HISTIP -c $IPCOSTS -o $OUTFILE
